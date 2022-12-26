8850fe5c1c0a788c97bee84404dc1b8ad12501de
mknyambura <kariuki.mercie@gmail.com>
2022-12-26 21:23:03 +0300

    update commit

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async(event) => {
   loader(messageDiv);
 
   // fecth data from the server 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://africode.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
